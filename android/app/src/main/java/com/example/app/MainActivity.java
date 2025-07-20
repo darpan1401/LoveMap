34e59eff5650b3c2fad6c68d8c4ca421dfc006a0
darpan1401 <darpandasani56@gmail.com>
2025-07-20 18:58:35 +0530

    app

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.lovemap;
+package com.example.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.lovemap',
+  appId: 'com.example.app',
   appName: 'LoveMap',
-  webDir: 'dist/findlove/browser'
+  webDir: 'dist/findLove/browser',
+  server: {
+    url: 'https://love-map-three.vercel.app/', // Your Vercel URL
+    cleartext: true
+  }
 };
 
 export default config;
