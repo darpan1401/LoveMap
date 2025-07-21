98e3a03db24c66d7565ebd34c6473f0ce00018de
darpan1401 <darpandasani56@gmail.com>
2025-07-21 20:21:37 +0530

    app

--- src/app/shared/notification/notification.component.ts
+++ src/app/shared/notification/notification.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from '../../services/notification.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-notification',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.css'
 })
